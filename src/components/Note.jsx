5dd58998f439c71105704934850211d16892bd28
Anica Rahman <anicarahman2009@yahoo.it>
2022-12-06 17:43:57 +0000

    part 1 complete

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Title</h1>
+      <p>A message for you.</p>
+    </div>
+  );
+}
+
+export default Note;
